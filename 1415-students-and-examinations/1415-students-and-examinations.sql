1d1c84a40874799c4d78f6dd7b78a1d00ce49ae6
Sakshi Raut <159616256+sakshiiraut@users.noreply.github.com>
2025-03-08 10:10:40 +0530

    Time: 966 ms (92.76%), Space: 0B (100%) - LeetHub

---
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+WITH AllCombinations AS (
+    SELECT s.student_id, s.student_name, sub.subject_name
+    FROM Students s
+    CROSS JOIN Subjects sub
+)
+SELECT 
+    ac.student_id, 
+    ac.student_name, 
+    ac.subject_name, 
+    COALESCE(COUNT(e.subject_name), 0) AS attended_exams
+FROM AllCombinations ac
+LEFT JOIN Examinations e
+ON ac.student_id = e.student_id AND ac.subject_name = e.subject_name
+GROUP BY ac.student_id, ac.student_name, ac.subject_name
+ORDER BY ac.student_id, ac.subject_name;
