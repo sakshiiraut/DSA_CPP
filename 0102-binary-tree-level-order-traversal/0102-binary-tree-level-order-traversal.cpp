ab819a2a5cfbfb4ea904971f9fa4b996b33132c4
Sakshi Raut <159616256+sakshiiraut@users.noreply.github.com>
2025-04-05 10:38:49 +0530

    Time: 5 ms (7.32%), Space: 16.9 MB (88.36%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -9,30 +9,59 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     vector<vector<int>> levelOrder(TreeNode* root) {
+//          vector<vector<int>> res;
+
+//         if (root == nullptr) return res;
+//         queue<TreeNode*>q;
+//         q.push(root);
+//         while(q.size()>0){
+//            int levelSize = q.size();
+//             vector<int> level;
+
+//             for (int i = 0; i < levelSize; i++) {
+//                 TreeNode* temp = q.front();
+//                 q.pop();
+
+//                 level.push_back(temp->val);
+
+//                 if (temp->left != nullptr) q.push(temp->left);
+//                 if (temp->right != nullptr) q.push(temp->right);
+//             }
+
+//             res.push_back(level); // Add current level to result
+//         }
+//         return res;
+//     }
+// };
 class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-         vector<vector<int>> res;
+    int levels(TreeNode* root) {
+        if (root == nullptr) return 0;
+        return 1 + max(levels(root->left), levels(root->right));
+    }
 
-        if (root == nullptr) return res;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(q.size()>0){
-           int levelSize = q.size();
-            vector<int> level;
-
-            for (int i = 0; i < levelSize; i++) {
-                TreeNode* temp = q.front();
-                q.pop();
-
-                level.push_back(temp->val);
-
-                if (temp->left != nullptr) q.push(temp->left);
-                if (temp->right != nullptr) q.push(temp->right);
-            }
-
-            res.push_back(level); // Add current level to result
+    void nthlevels(TreeNode* root, int curr, int level, vector<int> &lev) {
+        if (root == nullptr) return;
+        if (curr == level) {
+            lev.push_back(root->val);
+            return;
         }
-        return res;
+        nthlevels(root->left, curr + 1, level, lev);
+        nthlevels(root->right, curr + 1, level, lev);
+    }
+
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        int n = levels(root);
+
+        for (int i = 1; i <= n; i++) {
+            vector<int> lev;
+            nthlevels(root, 1, i, lev);
+            result.push_back(lev);
+        }
+        return result;
     }
 };
