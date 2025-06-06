80f48033ef8d04e9d15a207a75c879f7cfe0a722
Sakshi Raut <159616256+sakshiiraut@users.noreply.github.com>
2025-06-06 09:28:09 +0530

    Time: 35 ms (88.12%), Space: 34.7 MB (52.97%) - LeetHub

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -3,7 +3,7 @@ public:
     string robotWithString(string s) {
         int n = s.size();
         vector<char> minSuffix(n);
-        minSuffix[n - 1] = s[n - 1];
+        minSuffix[n-1] = s[n - 1];
 
         for (int i = n - 2; i >= 0; --i) {
             minSuffix[i] = min(s[i], minSuffix[i + 1]);
