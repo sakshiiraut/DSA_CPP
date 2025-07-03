1af33d52b0ff9bd9abe723a41df006ef7163808d
Sakshi Raut <159616256+sakshiiraut@users.noreply.github.com>
2025-07-03 10:04:36 +0530

    Time: 0 ms (100%), Space: 7.8 MB (96.34%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -1,15 +1,22 @@
+// class Solution {
+// public:
+//     char kthCharacter(int k) {
+//     string s="a";
+//     while(s.size()<=k){
+//         string temp="";
+//         for(int i=0;i<s.size();i++){
+//            if(s[i]<'z') temp+=char(s[i] + 1);
+//            else temp+='a';
+//         }
+//         s+=temp;
+//     } 
+//     return s[k-1];
+//     }
+// };
 class Solution {
 public:
     char kthCharacter(int k) {
-    string s="a";
-    while(s.size()<=k){
-        string temp="";
-        for(int i=0;i<s.size();i++){
-           if(s[i]<'z') temp+=char(s[i] + 1);
-           else temp+='a';
-        }
-        s+=temp;
-    } 
-    return s[k-1];
+       int x=__builtin_popcount(k-1);
+       return 'a'+x;
     }
 };
