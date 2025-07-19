2e5e4fe5830d3b69f53cd28ddcf8b115e7955eff
Sakshi Raut <159616256+sakshiiraut@users.noreply.github.com>
2025-07-19 15:10:10 +0530

    Time: 0 ms (100%), Space: 10.2 MB (74.12%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
+   string reverse(string word){
+    int i=0;
+    int n=word.size();
+    int j=n-1;
+    while(i<=j){
+        char temp=word[i];
+        word[i]=word[j];
+        word[j]=temp;
+        i++;
+        j--;
+    }
+    return word;
+   }
     string reverseWords(string s) {
-        int n=s.size();
-        stringstream ss(s);
-        string word;
-        string result;
-        stack<string>ch;
-    while (ss >> word) {  // Read words from the stream
-        ch.push(word);
-    }
-         while (!ch.empty()) {
-        result += ch.top();
-        ch.pop();
-        if (!ch.empty()) {
-            result += " "; 
+      string reversedString = reverse(s);  // reverse the full string
+        string word = "";
+        string ans = "";
+        int n = reversedString.size();
+
+        for (int i = 0; i <= n; ++i) {
+            // If current character is space or end of string
+            if (i == n || reversedString[i] == ' ') {
+                if (!word.empty()) {
+                    ans += reverse(word) + " ";
+                    word = "";
+                }
+            } else {
+                word += reversedString[i];
+            }
         }
+
+        // Remove trailing space if exists
+        if (!ans.empty() && ans.back() == ' ') {
+            ans.pop_back();
+        }
+
+        return ans;
     }
-    return result;
-  }
 };
