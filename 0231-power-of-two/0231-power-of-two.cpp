eb131a215d91d37f3f4e71af45e5b40a7bbce4b0
Sakshi Raut <159616256+sakshiiraut@users.noreply.github.com>
2025-08-09 15:13:05 +0530

    Time: 0 ms (100%), Space: 7.9 MB (52.81%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+       for(int i=0;i<=31;i++){
+        if(n==(1<<i)) return true;
+       } 
+       return false;
+    }
+};
