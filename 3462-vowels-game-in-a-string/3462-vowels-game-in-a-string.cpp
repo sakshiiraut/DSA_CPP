7717afe9d4d5a5bae7e96562dec366aaf712267e
Sakshi Raut <159616256+sakshiiraut@users.noreply.github.com>
2025-09-12 08:46:30 +0530

    Time: 3 ms (87.13%), Space: 18.9 MB (79.48%) - LeetHub

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        int count=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='a'|| s[i]=='e' || s[i]=='i' ||  s[i]=='o' || s[i]=='u' ) count++;
+        }
+        if(count==0) return false;
+        else return true;
+    }
+};
